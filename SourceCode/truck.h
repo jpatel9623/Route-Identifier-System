1dd8279a0400fe776b26e5fb8fefeabb2a40030d
Samarth Kamlesh Shah <skshah11@myseneca.ca>
2024-03-22 11:30:07 -0400

    added function declarations

--- SourceCode/truck.h
+++ SourceCode/truck.h
@@ -1,13 +1,21 @@
+#pragma once
 #ifndef TRUCK_H
 #define TRUCK_H
-#define MAX_CARGO_VOL 36      
-#define MAX_CARGO_WEIGHT 1000 
+#define MAX_CARGO_WEIGHT 1000
+#define MAX_CARGO_VOL 36    
+#include "mapping.h"
+
 
 struct Truck
 {
-    struct Route* route;
     int cargoWeight;
     double cargoVol;
+    struct Route* route;
+    
 };
+
+int AssignTruck(struct Shipment s);
+
+float calculateUtilizationScore(int weight, double vol);
 
 #endif
